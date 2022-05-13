64380e55cb862e6ca096d5eda667b0925165ff8f
chemonxd <jmcarbajal83@gmail.com>
2022-05-13 01:04:01 -0500

    Add files via upload

---
+++ Edad.py
@@ -0,0 +1,10 @@
+
+if __name__ == '__main__':
+
+    print("Por favor ingrese su edad en números enteros")
+    age = int(input())
+
+    if age <= 17:
+        print("Eres menor de edad")
+    else:
+        print("Eres mayor de edad")
